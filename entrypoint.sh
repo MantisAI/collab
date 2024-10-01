0038ffea098cc188e90c4bc792f3755f906a861e
agombert <arnault.gombert@pm.me>
2024-10-01 17:09:37 +0200

    :bug: try to fix bug with executing docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = "jupyter" ]; then
-    exec jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root
-else
-    exec "$@"
-fi
+exec jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root
